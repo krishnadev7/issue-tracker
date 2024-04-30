106c25533a03d912e2ca9857dc611729e22ed2b7
krishnadev7 <krishnadevv12@gmail.com>
2024-04-30 14:41:26 +0530

    created api for issue

--- issue-tracker/app/Navbar.tsx
+++ issue-tracker/app/Navbar.tsx
@@ -1,8 +1,15 @@
+"use client";
+
+import classNames from "classnames";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { BsBugFill } from "react-icons/bs";
 
 const Navbar = () => {
+  const currentPathname = usePathname();
+  console.log(currentPathname);
+
   const links = [
     {
       label: "Dashboard",
@@ -16,10 +23,22 @@ const Navbar = () => {
 
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 py-5 items-center">
-      <Link href="/"><BsBugFill/></Link>
+      <Link href="/">
+        <BsBugFill />
+      </Link>
       <ul className="flex space-x-6">
         {links.map((link) => (
-          <Link className="text-zinc-500 hover:text-black transition-colors" key={link.href} href={link.href}>{link.label}</Link>
+          <Link
+            className={classNames({
+              "text-zinc-900": link.href === currentPathname,
+              "text-slate-500": link.href != currentPathname,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
+            key={link.href}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
         ))}
       </ul>
     </nav>
